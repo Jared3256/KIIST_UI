22d97587dbebd8cf176fe0c594b948b59f83243c
Jared3256 <odhiambojared566@gmail.com>
2025-06-03 01:36:31 +0300

    cleaned landing page

--- src/pages/landing_page/LandingPage.tsx
+++ src/pages/landing_page/LandingPage.tsx
@@ -1,10 +1,5 @@
-import { CssBaseline, Divider } from "@mui/joy";
+import { CssBaseline } from "@mui/joy";
 import LandingPageBar from "../../components/landing_page/LandingPageBar";
-import Features from "../../components/landing_page/Features";
-import Testimonials from "../../components/landing_page/Testimonials";
-import Highlights from "../../components/landing_page/Highlights";
-import Pricing from "../../components/landing_page/Pricing";
-import FAQ from "../../components/landing_page/FAQ";
 import Footer from "../../components/landing_page/Footer";
 import Hero from "../../components/landing_page/Hero";
 import { Button, Layout } from "antd";
