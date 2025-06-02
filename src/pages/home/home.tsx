0fa193ce4b134b571e8649da15cf5d71580e0617
Jared3256 <odhiambojared566@gmail.com>
2025-06-03 01:40:07 +0300

    cleaned home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,14 +1,9 @@
 import { CssBaseline, Divider } from "@mui/joy";
-import React from "react";
 import { Outlet, useLocation } from "react-router";
 import FAQ from "src/components/landing_page/FAQ";
-import Features from "src/components/landing_page/Features";
 import Footer from "src/components/landing_page/Footer";
-import Hero from "src/components/landing_page/Hero";
-import Highlights from "src/components/landing_page/Highlights";
 import LandingPageBar from "src/components/landing_page/LandingPageBar";
 import Pricing from "src/components/landing_page/Pricing";
-import Testimonials from "src/components/landing_page/Testimonials";
 
 export default function Home() {
   const location = useLocation();
