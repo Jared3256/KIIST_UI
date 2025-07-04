b95b6664b1ece41b50c29a8313cf33a4c144deec
Jared3256 <odhiambojared566@gmail.com>
2025-07-04 09:48:52 +0300

    added analytics for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,23 @@
 import React from "react";
-import { lazy, StrictMode, Suspense } from "react";
-import { createRoot } from "react-dom/client";
+import {lazy, StrictMode, Suspense} from "react";
+import {createRoot} from "react-dom/client";
 import "./index.css";
 import PageLoader from "./components/PageLoader";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import store from "./redux/store";
+import {Analytics} from "@vercel/analytics/next"
 
 import "@ant-design/v5-patch-for-react-19";
 import Cursor from "./components/cursor";
+
 const KiistApp = lazy(() => import("./engine/KIIST"));
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode >
-   
-    <Suspense fallback={<PageLoader />}>
-      <Provider store={store}>
-        <KiistApp />
-      </Provider>
-    </Suspense>
-  </StrictMode>
+    <StrictMode><Analytics>
+        <Suspense fallback={<PageLoader/>}>
+            <Provider store={store}>
+                <KiistApp/>
+            </Provider>
+        </Suspense></Analytics>
+    </StrictMode>
 );
