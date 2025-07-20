68252ae60fa49d23ecf1241c27e2e30c3a3e9f5b
Jared3256 <odhiambojared566@gmail.com>
2025-07-20 11:31:09 +0300

    changed imahes to avif

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,22 +1,22 @@
-import { Box } from "@mui/joy";
-import { Image } from "antd";
-import Shan from "../assets/Shan.png";
-import { Loader } from "lucide-react";
+import {Box} from "@mui/joy";
+import {Image} from "antd";
+import Shan from "../assets/Shan.avif";
+import {Loader} from "lucide-react";
 
 export default function PageLoader() {
-  return (
-    <Box
-      height={"100vh"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      display={"flex"}
-      flexDirection={"column"}
-    >
-      <Image src={Shan} preview={false} width={300} height={300} />
+    return (
+        <Box
+            height={"100vh"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            display={"flex"}
+            flexDirection={"column"}
+        >
+            <Image src={Shan} preview={false} width={300} height={300}/>
 
-      <Box justifyContent={"center"} alignItems={"center"} display={"flex"}>
-        <Loader color={"blue"} className="w-6 h-6 animate-spin" />
-      </Box>
-    </Box>
-  );
+            <Box justifyContent={"center"} alignItems={"center"} display={"flex"}>
+                <Loader color={"blue"} className="w-6 h-6 animate-spin"/>
+            </Box>
+        </Box>
+    );
 }
