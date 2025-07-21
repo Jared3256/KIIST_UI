3967bed0ee8c060cbae4d9aa727ed426d19f1a0a
Jared3256 <odhiambojared566@gmail.com>
2025-07-21 13:16:15 +0300

    changed system_data for base url 2

--- src/config/serverApi.config.ts
+++ src/config/serverApi.config.ts
@@ -1,6 +1,6 @@
-const system_data ={
-    BASE_URL :import.meta.env.VITE_BASE_URL,
-    BASE_URL2:import.meta.env.VITE_BASE_URL_2,
+const system_data = {
+    BASE_URL: import.meta.env.VITE_BASE_URL,
+    BASE_URL2: import.meta.env.VITE_BASE_URL_2 || "https://kiist-core-production.up.railway.app/api/v1",
 }
 
 export default system_data
